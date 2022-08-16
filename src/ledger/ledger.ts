40029fec79e741e41af96a15393e25ad09d8799c
Pham Tu <tubackkhoa@gmail.com>
2022-08-16 13:47:07 +0700

    fix binding transport

--- src/ledger/ledger.ts
+++ src/ledger/ledger.ts
@@ -22,6 +22,7 @@ export const ledgerProxy = async (
         ledger = await LedgerInternal.init(mode, initArgs);
         response = true;
       } catch (error) {
+        console.log(error);
         response = false;
       }
       break;
