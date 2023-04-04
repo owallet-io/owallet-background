11b878eb84265b9ff6ec620d9a9e22675bae1ccc
sonlha <lhas1307@gmail.com>
2023-04-04 14:40:10 +0700

    check raw_data_hex

--- src/keyring/service.ts
+++ src/keyring/service.ts
@@ -825,7 +825,7 @@ export class KeyRingService {
 
       const receipt = await tronWeb.trx.sendRawTransaction(transaction);
       console.log('receipt ===', receipt);
-      return receipt.txid;
+      return receipt.txid ?? receipt.transaction.raw_data_hex;
     } finally {
       this.interactionService.dispatchEvent(
         APP_PORT,
