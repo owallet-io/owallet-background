b8dbf7ee516f43b3945f74214bbd6269c564ce58
sonlha <lhas1307@gmail.com>
2023-04-14 14:50:07 +0700

    spec test

--- src/ledger/ledger-app.spec.ts
+++ src/ledger/ledger-app.spec.ts
@@ -24,20 +24,20 @@ describe('Test ledger app', () => {
     const ethRet = await ethApp.getAddress("44'/60'/0'/0'/0");
     console.log(ethRet.address);
 
-    const cosmosApp = new CosmosApp(
-      await openTransportReplayer(
-        // second is public key return
-        RecordStore.fromString(`
-          => 5504000019046f7261692c00008076000080000000800000008000000000
-          <= 4104df00ad3869baad7ce54f4d560ba7f268d542df8f2679a5898d78a690c3db8f9833d2973671cb14b088e91bdf7c0ab00029a576473c0e12f84d252e630bb3809b28436241393833363265313939633431453138363444303932334146393634366433413634383435319000
-      `)
-      )
-    );
-    const cosmosRet = await cosmosApp.getAddress(
-      // Bip32Path.fromString("44'/118'/0'/0'/0").toPathArray(),
-      "44'/118'/0'/0'/0",
-      'orai'
-    );
-    console.log(cosmosRet.publicKey);
+    // const cosmosApp = new CosmosApp(
+    //   await openTransportReplayer(
+    //     // second is public key return
+    //     RecordStore.fromString(`
+    //       => 5504000019046f7261692c00008076000080000000800000008000000000
+    //       <= 4104df00ad3869baad7ce54f4d560ba7f268d542df8f2679a5898d78a690c3db8f9833d2973671cb14b088e91bdf7c0ab00029a576473c0e12f84d252e630bb3809b28436241393833363265313939633431453138363444303932334146393634366433413634383435319000
+    //   `)
+    //   )
+    // );
+    // const cosmosRet = await cosmosApp.getAddress(
+    //   // Bip32Path.fromString("44'/118'/0'/0'/0").toPathArray(),
+    //   "44'/118'/0'/0'/0",
+    //   'orai'
+    // );
+    // console.log(cosmosRet.publicKey);
   });
 });
